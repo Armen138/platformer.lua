880e0a8ca95d171c1ddb5190e7434125c4fb9337
Armen <armen138@gmail.com>
2014-12-28 01:15:14 +0100

    basic UI buttons and events

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 require("fizzx/fizz")
 console = require("src/console")
+ui = require("src/ui")
 map = require("src/map")
 scroll = require("src/scroll")
 system = require("src/system")
@@ -21,13 +22,47 @@ function toggleDebug()
     console.log('setting DEBUG mode ' .. debugStatus)
 end
 
+function pause()
+    ui.current = 'pause'
+end
+
+function help()
+    console.log('arrows to move, D for debug drawing, ESCAPE for menu')
+end
 function love.load() 
+    local font = love.graphics.newFont('ui/Font/kenvector_future.ttf')
+    love.graphics.setFont(font)
     handlers = {
-        escape = system.exit,
+        escape = pause, --system.exit,
+        h = help,
+        f1 = help,
         d = toggleDebug
     }
+    ui.load()
     map.load(fizz)
     player.load(fizz)
+    local center = {
+        x = love.window.getWidth() / 2,
+        y = love.window.getHeight() / 2
+    }
+    ui.menu('pause', {
+         -- panel = { label = '', sheet = 'grey', x = center.x, y = center.y - 110, style = 'panel', active = 'panel' },
+        resume = { label = 'resume', sheet = 'green', x = center.x, y = center.y - 100, style = 'button02', active = 'button03' },
+        restart = { label = 'restart', sheet = 'blue', x = center.x, y = center.y - 40, style = 'button02', active = 'button03' },
+        quit = { label = 'quit', sheet = 'red', x = center.x, y = center.y + 20, style = 'button01', active = 'button02' }
+    })
+    ui.on.click.quit = function(btn) 
+        system.exit()
+    end
+
+    ui.on.click.resume = function(btn)
+        ui.current = nil
+    end
+
+    ui.on.click.restart = function(btn)
+        ui.current = nil
+        console.log('TODO: restart')
+    end
 end
 
 function love.keypressed(key)
@@ -56,6 +91,7 @@ step = 0.016
 accum = 0
 
 function love.update(dt)
+    ui.update(dt)
     fizz.gravity = 1200
     accum = accum + dt
     while(accum > step) do
@@ -69,4 +105,7 @@ function love.draw()
     map.draw()
     player.draw()
     console.draw()
+    ui.draw()
+    --button.draw()
+    --button2.draw()
 end
---
+++ src/ui.lua
@@ -0,0 +1,106 @@
+local font
+local menus = {}
+local mouse = { x = 0, y = 0 }
+local defaultFont
+local sheets = {
+    blue = require("../ui/blueSheet"),
+    red = require("../ui/redSheet"),
+    yellow = require("../ui/yellowSheet"),
+    grey = require("../ui/greySheet"),
+    green = require("../ui/greenSheet")
+}
+
+local ui = {
+    on = {
+        click = {}
+    }
+}
+
+function ui.load() 
+    font = love.graphics.newFont('ui/Font/kenvector_future.ttf', 25)
+    defaultFont = love.graphics.newFont('ui/Font/kenvector_future.ttf', 10)
+    sheets.blue.image = love.graphics.newImage('ui/Spritesheet/blueSheet.png');
+    sheets.red.image = love.graphics.newImage('ui/Spritesheet/redSheet.png');
+    sheets.yellow.image = love.graphics.newImage('ui/Spritesheet/yellowSheet.png');
+    sheets.grey.image = love.graphics.newImage('ui/Spritesheet/greySheet.png');
+    sheets.green.image = love.graphics.newImage('ui/Spritesheet/greenSheet.png');
+    love.graphics.setFont(defaultFont)
+end
+
+function ui.menu(id, elements)
+    menus[id] = {}
+    for k, v in pairs(elements) do
+        menus[id][k] = ui.button(v.label, v.sheet, v.x, v.y, v.style, v.active)
+    end
+end
+
+function love.mousepressed(x, y, button)
+    if ui.current then
+        for k, v in pairs(menus[ui.current]) do
+            if v.isInside(x, y) then
+                if ui.on.click[k] then
+                    ui.on.click[k](button)
+                end
+            end
+        end
+    end
+end
+
+function ui.update(dt) 
+    mouse.x = love.mouse.getX()
+    mouse.y = love.mouse.getY()
+    if ui.current then
+        for k, v in pairs(menus[ui.current]) do
+            if v.isInside(mouse.x, mouse.y) then
+                if v.onHover then
+                    v.onHover()
+                end
+            end
+        end
+    end
+end
+
+function ui.draw() 
+    if ui.current then
+        for k, v in pairs(menus[ui.current]) do
+            v.draw()
+        end
+    end
+end
+
+function ui.button(text, sheet, left, top, style, active)
+    local hover = false
+    local function draw()
+        local quad = sheets[sheet][style]
+        if hover then
+            quad = sheets[sheet][active]
+        end
+        love.graphics.setFont(font)
+        local x, y, w, h = quad:getViewport()
+        love.graphics.draw(sheets[sheet].image, quad, left - w / 2, top - h / 2)            
+        love.graphics.printf(text, left - w / 2, top - h / 2 + 12, w, 'center')
+        love.graphics.setFont(defaultFont)
+    end
+    
+    local function isInside(sx, sy)
+        local x, y, w, h = sheets[sheet][style]:getViewport()
+        tx = left - w / 2
+        ty = top - h / 2 + 12
+        if sx > tx and
+            sx < tx + w and
+            sy > ty and
+            sy < ty + h then
+            hover = true
+            return true
+        end
+        hover = false
+        return false
+    end
+
+    local button = {
+        draw = draw,
+        isInside = isInside
+    }
+    return button
+end
+return ui
